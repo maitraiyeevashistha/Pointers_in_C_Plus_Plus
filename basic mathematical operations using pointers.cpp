f2d5133f3062a009b7161030997cb18735784364
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-08-07 02:21:24 -0400

    Create basic mathematical operations using pointers in array.cpp

---
+++ basic mathematical operations using pointers.cpp
@@ -0,0 +1,27 @@
+/EXPERIMENT-9
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={10,20,30,40,50};
+    int *P1 = &arr[2];
+    int *P2 = &arr[4];
+    
+    int diff= *P2-*P1;
+    cout <<"The difference between the elements is: "<<diff<<endl;
+    
+    int add= *P2+*P1;
+    cout <<"The addition between the elements is: "<<add<<endl;
+    
+    int multiplication= (*P2)*(*P1);
+    cout <<"The multiplication between the elements is: "<<multiplication<<endl;
+    
+}
+
+/*Output :
+The difference between the elements is: 20
+The addition between the elements is: 80
+The multiplication between the elements is: 1500
+*/
